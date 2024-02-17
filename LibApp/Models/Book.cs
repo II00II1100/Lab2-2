013bc127235d6263ee7216f07a3872c6bd9b1e79
goras <goras@XYZ>
2024-02-17 12:37:22 +0100

    1.

--- LibApp/Models/Book.cs
+++ LibApp/Models/Book.cs
@@ -5,16 +5,19 @@ namespace LibApp.Models
     public class Book
     {
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Podaj Tytuł")]
         [StringLength(100)]
         public string Title { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Podaj immie Autora")]
         public string Author { get; set; }
-        [Required]
         public Genre Genre { get; set; }
+        [Required(ErrorMessage = "Podaj Gatunek Książki")]
         public int GenreId { get; set; }
         public DateTime DateAdded { get; set; }
+        [Required(ErrorMessage = "Podaj date wydania")]
         public DateTime ReleaseDate { get; set; }
+        [Required(ErrorMessage = "Podaj ilość na stanie")]
+        [Range(1, 20, ErrorMessage = "Ilość min 1 | maks 20")]
         public byte NumberInStock { get; set; }
     }
 }
